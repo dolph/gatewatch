93217eae6f7d43410c0e0268764262dd093d4bca
Dolph Mathews <dolph.mathews@gmail.com>
2014-03-07 06:55:21 -0600

    drop instance config in favor of /etc/

--- gatewatch/__init__.py
+++ gatewatch/__init__.py
@@ -1,13 +1,13 @@
 import flask
 
 
-app = flask.Flask(__name__, instance_relative_config=True)
+app = flask.Flask(__name__)
 
 # start with a default configuration
 app.config.from_object('gatewatch.config')
 
 # override defaults with instance-specific configuration
-app.config.from_pyfile('gatewatch_config.py', silent=True)
+app.config.from_pyfile('/etc/gatewatch/gatewatch.conf.py', silent=True)
 
 
 import gatewatch.views  # flake8: noqa
