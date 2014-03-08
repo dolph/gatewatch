cca13b82720d6a31f9e94d0cec5fd11bd29617a4
Dolph Mathews <dolph.mathews@gmail.com>
2014-03-08 07:13:16 -0600

    add timers to test runner

--- test_vagrant.sh
+++ test_vagrant.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 vagrant destroy -f
-vagrant up
+time vagrant up
 curl -sL -w "%{http_code}\\n" http://192.168.111.222/data -o /dev/null | grep -q "200"
-vagrant provision
+time vagrant provision
 curl -sL -w "%{http_code}\\n" http://192.168.111.222/data -o /dev/null | grep -q "200"
