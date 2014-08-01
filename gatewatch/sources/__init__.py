2e0e4cc93ff6794d5e06a77bc443b29c0f4edaa8
Dolph Mathews <dolph.mathews@gmail.com>
2014-08-01 11:17:18 -0500

    add package import for graphite

--- gatewatch/sources/__init__.py
+++ gatewatch/sources/__init__.py
@@ -1,3 +1,4 @@
 from gatewatch.sources import gerrit  # noqa
+from gatewatch.sources import graphite  # noqa
 from gatewatch.sources import launchpad  # noqa
 from gatewatch.sources import zuul  # noqa
